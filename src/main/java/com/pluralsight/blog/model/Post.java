74a94ae3d85d4b5cbbe3e50a2b7a53f976db8d24
Daniel Codila <ionu.danic@gmail.com>
2020-08-16 09:33:58 +0200

    module 3 exercises

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -18,6 +18,11 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version private Long version;
+
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
@@ -28,6 +33,8 @@ public class Post {
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Author author;
+
+
 
     public Post() {
         super();
